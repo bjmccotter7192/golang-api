7435de39ef4362ed1c42389ffd981c7181f357cc
BJ McCotter <bj.mccotter@adhocteam.us>
2021-11-01 15:28:58 -0500

    Adding comments to define functions

--- main.go
+++ main.go
@@ -23,20 +23,23 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+// Returns a friendly STRING when system starts
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+// Returns a JSON formatted array of Albums
+func returnAllAlbums(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllAlbums")
+	json.NewEncoder(w).Encode(albums)
+}
+
+// Define all the routes and starts the server up
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/albums", returnAllAlbums)
 	log.Fatal(http.ListenAndServe(":8081", nil))
-}
-
-func returnAllAlbums(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllAlbums")
-	json.NewEncoder(w).Encode(albums)
 }
 
 func main() {
